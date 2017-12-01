a228bbdab91ee6ba8bd6acbf2f739183c9967b34
zhanglei53 <zhanglei53@jd.com>
2017-12-01 10:58:57 +0800

    test

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ const async = require('async');
 const request = require('request');
 
 var q = async.queue(function(task, callback) {
-    var orderId=task.orderId;
-    getOrderBank(orderId, callback);
-}, 1);
+    var orderId = task.orderId;
+    resetTask(orderId, callback);
+}, 3);
 
 // assign a callback
 q.drain = function() {
@@ -20,6 +20,27 @@ readFileLine.readLine('orderId', function (line) {
     });
 });
 
+// 二清 - 重置任务
+function resetTask(orderId, callback) {
+    var url = 'http://fm-bill.jd.com/service/accountTools/resetTask';
+
+    // 发送单参数且application/json的实践
+    request({
+        url: url,
+        method: 'POST',
+        body: orderId,
+        headers: {
+            'Content-Type':'application/json;charset=UTF-8'
+        },
+        // proxy
+        proxy: 'http://127.0.0.1:8888'
+    }, function (err, res, body) {
+        console.log(orderId + '_' + body);
+        callback();
+    })
+}
+
+// 查询台账
 function getOrderBank(orderId, callback) {
     var url = 'http://orderbank.soa.jd.local/service/orderQuery/getOrderBank';
 
